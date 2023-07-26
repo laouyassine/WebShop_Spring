503efa631c5b14455c33025f0323dca5cc7b8eb7
François Caremoli <francois.caremoli@wanadoo.fr>
2023-07-26 23:33:00 +0200

    Mise à jour classes Example

--- src/main/java/fr/maboite/webshop/spring/SpringJpaApplication.java
+++ src/main/java/fr/maboite/webshop/spring/SpringJpaApplication.java
@@ -13,7 +13,6 @@ public class SpringJpaApplication {
 				= new AnnotationConfigApplicationContext(
 				SpringConfiguration.class)) {
 			
-			
 		}
 	}
 
--- src/test/java/fr/maboite/webshop/service/ExampleServiceTest.java
+++ src/test/java/fr/maboite/webshop/service/ExampleServiceTest.java
@@ -1,7 +1,5 @@
 package fr.maboite.webshop.service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import fr.maboite.webshop.spring.real.configuration.SpringConfiguration;
@@ -9,12 +7,5 @@ import fr.maboite.webshop.spring.real.configuration.SpringConfiguration;
 @SpringJUnitConfig(SpringConfiguration.class)
 public class ExampleServiceTest {
 	
-	@Autowired
-	private ExampleService exampleService;
-
-	@Test
-	public void testFindByNom() {
-		exampleService.findByNom("test");
-	}
 
 }
